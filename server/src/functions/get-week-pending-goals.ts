2116374d48c726dae5b705efb5ff8acfeb178181
Lucas <lucas.primati@hotmail.comgit config --global user.email lucas.primati@hotmail.com>
2024-09-12 14:58:58 -0300

    adicionando get week pending goals

--- server/src/functions/get-week-pending-goals.ts
+++ server/src/functions/get-week-pending-goals.ts
@@ -1,3 +1,33 @@
-export function getWeeekPendingGoals(){
-    
+import dayjs from "dayjs";
+import weekOfYear from "dayjs/plugin/weekOfYear";
+import { db } from "../db";
+import { and, count, lte, sql } from "drizzle-orm";
+import { goalCompletions, goals } from "../db/schema";
+
+dayjs.extend(weekOfYear);
+
+export function getWeeekPendingGoals() {
+	const lastDayOfWeek = dayjs().endOf("week").toDate();
+
+	const goalsCreatedUpToWeek = db.$with("goals_created_up_to_week").as(
+		db
+			.select({
+				id: goals.id,
+				title: goals.title,
+				desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+				createdAt: goals.createdAt,
+			})
+			.from(goals)
+			.where(lte(goals.createdAt, lastDayOfWeek)),
+	);
+
+	const goalCompletionCounts = db.$with("goal_completion_counts").as(
+		db
+			.select({
+				goalId: goalCompletions.goalId,
+				compleitonCount: count(goalCompletions.id),
+			})
+			.from(goalCompletions)
+			.groupBy(goalCompletions.goalId),
+	);
 }
