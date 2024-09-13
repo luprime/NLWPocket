16d6f017897e3ea810cd81ebab247fb04a43cd48
Lucas <lucas.primati@hotmail.comgit config --global user.email lucas.primati@hotmail.com>
2024-09-13 19:19:19 -0300

    finalizaçao do backend.

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -7,10 +7,14 @@ import {
 import { createGoalRoute } from "./routes/create-goals";
 import { createCompletionRoute } from "./routes/create-completion";
 import { getPendingGoalsRoute } from "./routes/get-pending-goals";
-import { getWeekSummary } from "../functions/get-week-summary";
 import { getWeekSummaryRoute } from "./routes/get-week-summary";
+import fastifyCors from "@fastify/cors";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+	origin: "*",
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
